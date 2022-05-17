eb00242e7d31899912794f444894a178fb58fadc
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-17 14:56:59 -0500

    fixed animation

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var projects = document.querySelector("#pPage");
 var projectBtn = document.querySelector(".projectButton");
 
 projectBtn.addEventListener('click', (e) => {
-    projects.scrollIntoView({behavior: "smooth", block: "end"});
+    projects.scrollIntoView(false, {behavior: "smooth"});
 });
