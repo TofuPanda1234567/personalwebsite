b40318254d62619abbfc15a65bcb7cc65f28d88c
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-17 14:53:15 -0500

    testing animation on click

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-var elmntToView = document.getElementById("pPage");
-elmntToView.scrollIntoView({behavior: "smooth"});
+var projects = document.querySelector("#pPage");
+var projectBtn = document.querySelector(".projectButton");
+
+projectBtn.addEventListener('click', (e) => {
+    projects.scrollIntoView({behavior: "smooth"});
+});
