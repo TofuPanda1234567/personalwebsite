4da816692f236045956da1399ab4da2a9ee4b3f8
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-17 15:23:52 -0500

    added contact

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var projects = document.querySelector("#pPage");
-var btn = document.querySelector(".projectButton");
+var email = document.querySelector(".cPage");
+var projectBtn = document.querySelector(".projectButton");
+var contactBtn = document.querySelector(".contactButton");
+var emailBtn = document.querySelector('.emailButton');
 
-btn.addEventListener('click', function () {
+
+// PROJECT BUTTON
+projectBtn.addEventListener('click', function () {
     projects.scrollIntoView({behavior: "smooth", block: "end"});
 });
+
+// EMAIL BUTTON
+emailBtn.addEventListener('click', (e) => {
+  window.open('mailto:christopherlocascio264@gmail.com', '_blank');
+});
+
+
