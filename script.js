714e0d1cf235795b7c86d77d2558179f6679a6c9
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-17 14:55:06 -0500

    animation scrolls to end

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var projects = document.querySelector("#pPage");
 var projectBtn = document.querySelector(".projectButton");
 
 projectBtn.addEventListener('click', (e) => {
-    projects.scrollIntoView({behavior: "smooth"});
+    projects.scrollIntoView({behavior: "smooth", block: "end"});
 });
