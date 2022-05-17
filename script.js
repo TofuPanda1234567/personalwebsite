353b91416e3e4e85b560f3d73800367f3b31deef
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-17 15:09:21 -0500

    testing animation

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var projects = document.querySelector("#pPage");
 var btn = document.querySelector(".projectButton");
 
 btn.addEventListener('click', function () {
-    projects.scrollIntoView(false, {behavior: "smooth"});
+    projects.scrollIntoView({behavior: "smooth", block: "end"});
 });
